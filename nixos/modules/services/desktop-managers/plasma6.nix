6556e765ebebd68ce68410cbd21a4601eb23cba8
K900 <me@0upti.me>
2026-06-08 23:12:43 +0300

    kdePackages: Plasma 6.6.5 -> 6.7 (beta 2)
    Co-authored-by: 𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
    Co-authored-by: adisbladis <adisbladis@gmail.com>

--- nixos/modules/services/desktop-managers/plasma6.nix
+++ nixos/modules/services/desktop-managers/plasma6.nix
@@ -169,6 +169,9 @@ in
           krdp
           kconfig # required for xdg-terminal from xdg-utils
           qtbase # for qtpaths which is required for xdg-mime from xdg-utils
+          # touch keyboard
+          plasma-keyboard
+          qtvirtualkeyboard # used by plasma-keyboard KCM
 
           # experimental(?) Union theme
           union
